8dbf529eaa76242cf332b79c6498c6f5c9c048a9
ThanSoryalyga <thann.soryalyza@institute.pse.ngo>
2026-03-26 16:25:43 +0700

    Finising on homepage

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+// Make sure it says 'export default function'
+export default function Header() {
+  return (
+    <div>
+      <img src="/logo.jpg" alt="Logo" />
+      <h1>Hello</h1>
+    </div>
+  );
+}
